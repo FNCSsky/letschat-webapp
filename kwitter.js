135f84681823c4f480ffe85530d5737718fe5fee
FNCSsky <97810010+FNCSsky@users.noreply.github.com>
2023-12-07 17:42:00 -0500

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -10,3 +10,27 @@ function addUser() {
 
 }
 
+function addRoom() {
+
+
+    
+}
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+<div>
+    ""          
+        </div>
+//End code
+});});}
+getData();
+
+function redirectToRoomName () {
+
+
+    
+}
+
